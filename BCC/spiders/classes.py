14011456432dba0e2b9ac66dabce105ba11366eb
Ayrton Vitor <ayrtonvitor@pm.me>
2022-10-25 17:28:10 -0300

    begin implementation of crawler

---
+++ BCC/spiders/classes.py
@@ -0,0 +1,17 @@
+import scrapy
+import os
+
+class ClassesSpider(scrapy.Spider):
+    name = "classes"
+
+    def start_requests(self):
+        URLS_PATH = os.path.join(os.path.dirname(__file__), "URLS")
+        urls = []
+        with open(URLS_PATH, 'r') as f:
+            urls.append(f.readline().strip())
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        pass
